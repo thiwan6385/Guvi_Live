90144e2318f4662b3117a7fd8ade7edc2b103124
thiwan6385 <spidethibu1101@gmail.com>
2025-02-13 19:59:56 +0530

    first-commit

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,12 @@
+let fruits=["Apple","Mangoes","Pine Apple"]
+
+console.log(fruits.length)
+
+let basket=fruits.map((fruit) => fruit + "juice");
+console.log(basket);
+
+let studets=["Thiwan","Nalini","Banu","Raja","Raji","Rajes"]
+
+for (i=0; i<studets.length; i++) {
+    
+}
